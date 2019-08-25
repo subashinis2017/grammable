08da257f7130094d032cf52bfa62e619e94e9a19
Subashini <subashinis2017@gmail.com>
2019-08-25 23:11:38 +0000

    Added FactoryBot changes

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,11 +16,7 @@ RSpec.describe GramsController, type: :controller do
     end
   
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -34,36 +30,27 @@ RSpec.describe GramsController, type: :controller do
         post :create, params: { gram: { message: "Hello" } }
         expect(response).to redirect_to new_user_session_path
     end
-    
+
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
-      sign_in user
+        user = FactoryBot.create(:user)
+        sign_in user
 
-      post :create, params: { gram: { message: 'Hello!' } }
-      expect(response).to redirect_to root_path
+        post :create, params: { gram: { message: 'Hello!' } }
+        expect(response).to redirect_to root_path
 
-      gram = Gram.last
-      expect(gram.message).to eq("Hello!")
-      expect(gram.user).to eq(user)
+        gram = Gram.last
+        expect(gram.message).to eq("Hello!")
+        expect(gram.user).to eq(user)
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
-      sign_in user
+        user = FactoryBot.create(:user)
+        sign_in user
 
-      gram_count = Gram.count
-      post :create, params: { gram: { message: '' } }
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(gram_count).to eq Gram.count
+        gram_count = Gram.count
+        post :create, params: { gram: { message: '' } }
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(gram_count).to eq Gram.count
     end
-
   end
 end
