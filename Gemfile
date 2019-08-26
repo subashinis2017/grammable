62aeaea6679479ff5e835b36cd88ca84b0c8d9a2
Subashini <subashinis2017@gmail.com>
2019-08-26 16:44:47 +0000

    Added code for image upload

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'jquery-rails'
 gem 'bootstrap', '>= 4.3.1'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_190717) do
+ActiveRecord::Schema.define(version: 2019_08_26_030805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_25_190717) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
